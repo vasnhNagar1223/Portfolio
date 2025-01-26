f5bb4d010294b4c7b6462cd885b8486a4dbc5e7e
Vansh Nagar <vansh.nagar.dev@gmail.com>
2025-01-27 00:08:42 +0530

    update1

--- script/index.js
+++ script/index.js
@@ -105,3 +105,57 @@ p2tl.from(".list", {
   scale: 0.5,
   duration: 4,
 });
+
+//page4
+gsap.from(".PROJECT-CONTAINER div", {
+  y: 100,
+  stagger: 0.1,
+  filter: "blur(2px)",
+  scale: 0,
+  transform: "rotate(50deg)",
+  scrollTrigger: {
+    trigger: ".page4seemywork",
+    scrub: 2,
+    start: "top 80%",
+    end: "top 10%",
+  },
+});
+
+//page5
+gsap.to(".page5EDUCATION", {
+  transform: " translateX(-85%)",
+  scrollTrigger: {
+    trigger: ".page5",
+    end: "top -800%",
+    scrub: 3,
+    pin: true,
+  },
+});
+
+//page6
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  effect: "coverflow", // Enable Coverflow effect
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50, // Rotation angle
+    stretch: 0, // Space between slides in px
+    depth: 100, // Depth of the slide effect
+    modifier: 1, // Multiplier for depth and rotate values
+    slideShadows: true, // Enable shadows
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
